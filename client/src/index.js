ec925b8cad4f23274dfa551a02365f90bf9dd21d
Max <maksym@smartrr.com>
2024-12-25 20:27:15 +0200

    front-end setup

---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from "./components/App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+
+root.render(
+    <StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </StrictMode>,
+    document.getElementById("root")
+)
