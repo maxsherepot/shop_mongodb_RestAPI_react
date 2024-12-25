906b4c7aa977170dc0136ad8cc47da8a7fb5a84f
Max <maksym@smartrr.com>
2024-12-25 14:56:26 +0200

    "config file added"

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
+const config = require("./config.json")
 const productsRoutes = require("./routes/products")
 const MONGO_DB_URI = "mongodb+srv://dominator5530:dominator5530@node-mongo-cluster.kze0g.mongodb.net/"
 
@@ -9,6 +10,6 @@ const app = express()
 
 app.use(productsRoutes)
 
-mongoose.connect(MONGO_DB_URI)
-    .then(() => app.listen(8000))
+mongoose.connect(config.mongoUri)
+    .then(() => app.listen(config.port))
     .catch(err => console.log(err))
