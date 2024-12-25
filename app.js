7bd782c2fe4c9b5452134dc265e2fbbeb95644e8
Max <maksym@smartrr.com>
2024-12-25 12:16:05 +0200

    first route and controller added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
-
-const app = express()
+const productsRoutes = require("./routes/products")
 const MONGO_DB_URI = "mongodb+srv://dominator5530:dominator5530@node-mongo-cluster.kze0g.mongodb.net/"
 
+
+const app = express()
+
+app.use(productsRoutes)
 
 mongoose.connect(MONGO_DB_URI)
     .then(() => app.listen(8000))
