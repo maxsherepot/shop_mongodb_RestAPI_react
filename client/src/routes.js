a57767e88315cede5232c3c2bda434509a99a151
Max <maksym@smartrr.com>
2024-12-27 17:29:50 +0200

    navbar component added

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,17 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import ProductDetailsPage from './pages/productDetailsPage';
 import ProductsPage from './pages/productsPage';
 
 
 const Routes = () => (
-    <Switch>
-        <Route exact path="/" component={ProductsPage} />
-        <Route path="/:id" component={ProductDetailsPage} />
-    </Switch>
+    <>
+        <Navbar />
+        <Switch>
+            <Route exact path="/" component={ProductsPage} />
+            <Route path="/:id" component={ProductDetailsPage} />
+        </Switch>
+    </>
 )
 
 export default Routes;
