c957015c86198c8664bebb4e869b79e963cf0bab
Max <maksym@smartrr.com>
2024-12-28 20:41:55 +0200

    Add new product form UI added

--- client/src/pages/addProductPage.jsx
+++ client/src/pages/addProductPage.jsx
@@ -19,7 +19,39 @@ const AddProductPage = () => {
 
     return (
         <div className="container">
-            <h1>AddProductPage</h1>
+            <h2>Add new product</h2>
+
+            <div className="new-product-container mt-5">
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="title" className="form-label mb-1">
+                            Title
+                        </label>
+                        <input type="text" className="form-control" id="title" />
+                        <div className="form-text">Some input info text</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="price" className="form-label mb-1">
+                            Price
+                        </label>
+                        <input type="number" className="form-control" id="price" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="imageUrl" className="form-label mb-1">
+                            Image url
+                        </label>
+                        <input type="text" className="form-control" id="imageUrl" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="description" className="form-label mb-1">
+                            Description
+                        </label>
+                        <textarea type="text" className="form-control" id="description" />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary shadow">Create</button>
+                </form>
+            </div>
         </div>
     )
 }
